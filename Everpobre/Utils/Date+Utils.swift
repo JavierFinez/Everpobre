a8de2c35ec4236f468b3fa893b34b8c734dd1281
Javier Finez <javierfinezdios@gmail.com>
2018-12-30 13:28:19 +0100

    Note Details implemented

---
+++ Everpobre/Utils/Date+Utils.swift
@@ -0,0 +1,9 @@
+//
+//  Date+Utils.swift
+//  Everpobre
+//
+//  Created by Javier Finez de Dios on 24/12/18.
+//  Copyright © 2018 Javier Finez de Dios. All rights reserved.
+//
+
+import Foundation
